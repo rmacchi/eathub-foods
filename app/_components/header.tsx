37ba0fc86f55129acdf73c477df7fd29291e7934
Rafael Macchi <rafaelmacchi199@gmail.com>
2024-05-23 10:31:27 -0300

    feat: add promo banner to homepage and change the logo

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,7 +5,7 @@ import { MenuIcon } from "lucide-react";
 const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-4">
-      <Image src="/logo.png" alt="eathub food" height={30} width={110} />
+      <Image src="/logo-teste3.png" alt="eathub food" height={30} width={110} />
 
       <Button
         size="icon"
